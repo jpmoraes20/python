8d3d3b995b4585ed2053605d08322a3e64905887
João Paulo Silva Moraes <84606902+jpmoraes20@users.noreply.github.com>
2022-09-29 00:07:35 -0300

    Add files via upload

---
+++ EscadaComString.py
@@ -0,0 +1,5 @@
+nome = input('Digite seu nome: ')
+#lista = list(nome)
+for i in range(len(nome)):
+    print(nome)
+    nome = nome[:-1]
---
+++ conversaoAMPM.py
@@ -0,0 +1,18 @@
+'''hora = int(input('Digite a hora: '))
+minutos = int(input('Digite os minutos: '))
+
+def conversao(h):
+    if h > 12:
+        h-=12
+        print(f'Agora são {h}:{minutos} P.M')
+    else:
+        h = h
+        print(f'Agora são {h}:{minutos} A.M')
+        
+conversao(hora)'''
+def soma(x,y):
+    num = x+y
+    return num
+print(soma(10,20))
+#print(num)
+    
